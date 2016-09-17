37766ebe0a3d998ae2b3125e441628680fff147b
Garrett Heaver <garrett@iterationfour.com>
2016-09-17 19:46:29 +0100

    fixing problem with PATH env inside container

--- postgres/build.sh
+++ postgres/build.sh
@@ -6,6 +6,7 @@ export BASE_IMAGE=${BASE_IMAGE:-armv7/armhf-ubuntu:xenial}
 export GOSU_VERSION=${GOSU_VERSION:-1.7}
 export LANG=${LANG:-en_US.utf8}
 export GNUPGHOME="\$GNUPGHOME"
+export BASEPATH="\$PATH"
 
 cat Dockerfile.armhf | envsubst > Dockerfile
 docker build --tag "garrettheaver/armhf-postgres:${PG_MAJOR}" . && \
