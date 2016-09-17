15c45be7c18b691597e15043b25786b1cddd83b9
Garrett Heaver <garrett@iterationfour.com>
2016-09-17 19:25:05 +0100

    updated build script to allow variable overrides

--- postgres/build.sh
+++ postgres/build.sh
@@ -2,10 +2,10 @@
 
 export PG_VERSION=$1
 export PG_MAJOR=${PG_VERSION:0:3}
-export BASE_IMAGE="armv7/armhf-ubuntu:xenial"
+export BASE_IMAGE=${BASE_IMAGE:-armv7/armhf-ubuntu:xenial}
+export GOSU_VERSION=${GOSU_VERSION:-1.7}
+export LANG=${LANG:-en_US.utf8}
 export GNUPGHOME="\$GNUPGHOME"
-export GOSU_VERSION="1.7"
-export LANG="en_US.utf8"
 
 cat Dockerfile.armhf | envsubst > Dockerfile
 docker build --tag "garrettheaver/armhf-postgres:${PG_MAJOR}" . && \
