323bb4387b7bb66412917acdf0ff908768121148
Garrett Heaver <garrett@iterationfour.com>
2016-09-17 23:01:56 +0100

    added comments to the postgres build script

--- postgres/build.sh
+++ postgres/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# This script is used to build the armhf variant of PostgreSQL and uses the
+# appropriate version of the armhf-ubuntu container as a base. It must be
+# executed on an arm device with a running Docker service which has already
+# been authenticated to Docker Hub via `docker login`. The Dockerfile itself
+# is based off the official 'postgres' Dockerfile with only very minor
+# modifications.
+
 export PG_VERSION=$1
 export PG_MAJOR=${PG_VERSION:0:3}
 export BASE_IMAGE=${BASE_IMAGE:-armv7/armhf-ubuntu:xenial}
