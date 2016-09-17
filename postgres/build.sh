cb6ae9fa18527cf725619d2399b06805cd861c2b
Garrett Heaver <garrett@iterationfour.com>
2016-09-17 18:40:06 +0100

    added proper build.sh file for armhf-postgres

---
+++ postgres/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export PG_VERSION=$1
+export PG_MAJOR=${PG_VERSION:0:3}
+export BASE_IMAGE="armv7/armhf-ubuntu:xenial"
+export GOSU_VERSION="1.7"
+export LANG="en_US.utf8"
+
+cat Dockerfile | envsubst | \
+  docker build --tag "garrettheaver/armhf-postgres:${PG_MAJOR}" - && \
+  docker push "garrettheaver/armhf-postgres:${PG_MAJOR}"
+
