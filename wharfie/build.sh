78380ef49b6cd61a7f73074058c9d7bbce057dbd
Garrett Heaver <garrett@iterationfour.com>
2016-09-17 20:04:04 +0100

    added build.sh script for armhf-wharfie container

---
+++ wharfie/build.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# This script is used to build the armhf variant of Wharfie and uses the
+# appropriate version of the armhf-alpine container as a base. It must be
+# executed on an arm device with a running Docker service which has already
+# been authenticated to Docker Hub via `docker login`
+
+DOCKER_TAG=${DOCKER_TAG:-garrettheaver/armhf-wharfie}
+BASE_IMAGE=${BASE_IMAGE:-container4armhf/armhf-alpine}
+
+# line up the files
+curl -s https://raw.githubusercontent.com/garrettheaver/wharfie/master/Dockerfile | \
+  sed -e 's|FROM .*:|FROM '"$BASE_IMAGE"':|g' > Dockerfile && \
+  docker build --tag="$DOCKER_TAG" . && \
+  docker push "$DOCKER_TAG" && \
+  rm Dockerfile
+
